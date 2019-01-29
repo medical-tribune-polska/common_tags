fa1cce6d8f2debd2923aa26b850b9b0d45bd77ec
Dmytro Koval <d.koval@medical-tribune.pl>
2019-01-29 15:24:09 +0100

    Search by all tags

--- app/models/common_tags/taggable.rb
+++ app/models/common_tags/taggable.rb
@@ -9,7 +9,7 @@ module CommonTags
                class_name: 'CommonTags::Tagging',
                dependent: :destroy
 
-      scope :with_tags, lambda { |tag_ids|
+      scope :with_any_tag, lambda { |tag_ids|
         joins <<-SQL
           INNER JOIN (
             SELECT DISTINCT(taggable_id)
@@ -20,6 +20,23 @@ module CommonTags
           ON #{table_name}.id = selected_taggings.taggable_id
         SQL
       }
+
+      scope :with_tags, lambda { |tag_ids|
+        joins(tag_ids.map { |tag_id| CommonTags::Taggable.join_statement(tag_id, name, table_name) }.join)
+      }
+    end
+
+    def self.join_statement(tag_id, name, table_name)
+      unique_join_name = SecureRandom.hex
+      <<-SQL
+        INNER JOIN (
+          SELECT DISTINCT(taggable_id)
+          FROM common_tags_taggings
+          WHERE common_tags_taggings.taggable_type = '#{name}'
+          AND common_tags_taggings.tag_id = '#{tag_id}'
+        ) selected_#{unique_join_name}
+        ON #{table_name}.id = selected_#{unique_join_name}.taggable_id
+      SQL
     end
   end
 end
